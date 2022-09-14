81d6d32c5a3906dd0f49c89cc5250ae5d60c7983
dlanaras <dimitrislanaras04@outlook.com>
2022-09-14 09:14:41 +0000

    hard optional excercise

--- src/main/java/ch/zli/m223/controller/EntryController.java
+++ src/main/java/ch/zli/m223/controller/EntryController.java
@@ -11,6 +11,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.Status;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
@@ -37,6 +38,8 @@ public class EntryController {
     @Consumes(MediaType.APPLICATION_JSON)
     @Operation(summary = "Creates a new entry.", description = "Creates a new entry and returns the newly added entry.")
     public Entry create(Entry entry) {
+        if (entry.getCheckOut().isBefore(entry.getCheckIn()))
+            return null;
         return entryService.createEntry(entry);
     }
     
