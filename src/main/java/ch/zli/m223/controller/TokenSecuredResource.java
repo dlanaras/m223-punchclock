760a4f7f2161f288483371816f031c3655d260cc
dlanaras <dimitrislanaras04@outlook.com>
2022-09-21 08:20:07 +0000

    hardcoded for life

--- src/main/java/ch/zli/m223/controller/TokenSecuredResource.java
+++ src/main/java/ch/zli/m223/controller/TokenSecuredResource.java
@@ -3,9 +3,12 @@ package ch.zli.m223.controller;
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -13,12 +16,15 @@ import javax.ws.rs.core.SecurityContext;
 
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
+import ch.zli.m223.GenerateToken;
+import ch.zli.m223.model.Login;
+
 @Path("/secured")
 public class TokenSecuredResource {
     @Inject
     JsonWebToken jwt;
 
-    @GET
+    @GET()
     @Path("permit-all")
     @PermitAll
     @Produces(MediaType.TEXT_PLAIN)
@@ -43,12 +49,25 @@ public class TokenSecuredResource {
         } else {
             name = ctx.getUserPrincipal().getName();
         }
-
-        return String.format("hello + %s," + " isHttps: %s," + " authScheme: %s" + " hasJWT: %s", name, ctx.isSecure(),
-                ctx.getAuthenticationScheme(), hasJwt());
+        return String.format("hello + %s,"
+                + " isHttps: %s,"
+                + " authScheme: %s,"
+                + " hasJWT: %s",
+                name, ctx.isSecure(), ctx.getAuthenticationScheme(), hasJwt());
     }
 
     private boolean hasJwt() {
         return jwt.getClaimNames() != null;
+    }
+
+    // THIS IS PEAK SECURITY
+    @Path("/login")@POST @Consumes(MediaType.APPLICATION_JSON)@Produces(MediaType.TEXT_PLAIN)
+
+    public String simulateLogin(Login login) {
+        if (login.getPassword().equals("secure") && login.getName().equals("admin")) {
+            return GenerateToken.getMockToken();
+        } else {
+            return "wrong";
+        }
     }
 }
